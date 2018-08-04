25cc21fa4175c85163ebb82b55872a2506d0655d
Maximilianus Paredes <max0720x@gmail.com>
2018-08-04 11:17:03 -0400

    RtnNonRcrtChar

---
+++ ReturnTheNonRecurentCharacter.js
@@ -0,0 +1,18 @@
+let finder = (str) => {
+  const charMap = {}
+  for (let i =0; i < str.length +1; i++) {
+    if (i === str.length) {
+      const index = Object.values(charMap).indexOf(1);
+      return Object.keys(charMap)[index]
+      
+    } else if (!charMap[str[i]]){
+    charMap[str[i]] = 1;
+    } else {
+      charMap[str[i]]++;
+    } 
+  }
+
+  //return charMap;
+}
+
+finder("APAPOAKOSAP");
